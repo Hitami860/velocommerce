e98ae85d418f295aaa07638beb9b4c89d51fa7c0
Hitami860 <Hitami86000@gmail.com>
2024-10-23 16:47:28 +0200

    upd

---
+++ public/navburger.js
@@ -0,0 +1,10 @@
+// let bttn = document.getElementById('bttn')
+// let navigation = document.getElementById('navigation')
+// let appears = bttn.addEventListener('click', () => {
+//     if (navigation.classList.contains('hidden')) {
+//         navigation.classList.remove('hidden')
+//     }
+//     else {
+//         navigation.classList.add('hidden')
+//     }
+// });
